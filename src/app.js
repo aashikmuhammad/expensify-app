28f475ba2d175c000ae93f2e1cbbafc5ce7fa4c0
aashiq_muha <aashiqmuhammad4@gmail>
2020-08-22 19:53:40 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -10,16 +10,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 650, createdAt: 1000}))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 750}))
-store.dispatch(addExpense({ description: 'Rent', createdAt: 750}))
-
-
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-console.log(visibleExpenses)
-
 const jsx =  (
     <Provider store={store}>
         <AppRouter />
