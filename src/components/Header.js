879207e9af75c009eec1bbe8c0113ea8bc90b271
aashiq_muha <aashiqmuhammad4@gmail>
2020-08-22 21:31:58 +0530

    first commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,6 @@ const Header = () => (
         <h1>Expensify</h1>
         <NavLink to="/" activeClassName="is-active" exact={true}>dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Add Expense</NavLink>
-        <NavLink to="/edit/12" activeClassName="is-active">Edit Expense</NavLink>
         <NavLink to="/help" activeClassName="is-active"> Help Page</NavLink>
     </header>
 )
