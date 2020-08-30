ed98473523e4b4d2a206e0408c4c497d36ecfc08
aashiq_muha <aashiqmuhammad4@gmail>
2020-08-30 13:15:03 +0530

    fetch add from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,22 +1,29 @@
-import {v1 as uuidv1} from 'uuid';
+// import {v1 as uuidv1} from 'uuid';
+import database from '../firebase/firebase';
 
-export const addExpense = (
-    { 
-        description='',
-        note='',
-        amount=0,
-        createdAt=0
-    } = {}
-) => ({
+export const addExpense = (expenses) => ({
     type: 'ADD_EXPENSE',
-    expenses: {
-        id: uuidv1(),
-        description,
-        note,
-        amount,
-        createdAt,
-    }
+    expenses
 });
+
+export const startAddExpense = (expenseData = {}) => {
+    return (dispatch) => {
+        const {
+            description='',
+            note='',
+            amount=0,
+            createdAt=0
+        } = expenseData;
+        const expense = { description, note, amount, createdAt}
+
+        database.ref('expenses').push(expense).then((ref) => {
+            dispatch(addExpense({ 
+                ...expense,
+                id: ref.key
+            }))
+        })
+    }
+}
 
 export const removeExpense = ({ id }= {}) => ({
     type: 'REMOVE_EXPENSE',
@@ -28,3 +35,25 @@ export const editExpense = (id, updates) => ({
     id,
     updates
 })
+
+export const setExpense = (expenses) => ({
+    type: 'SET_EXPENSES',
+    expenses
+})
+
+export const startSetExpense = () => {
+    return (dispatch) => {
+        return database.ref('expenses').once('value').then((snapshot) => {
+                const expenses = [];
+
+                snapshot.forEach((childSnapshot) => {
+                    expenses.push({
+                        id: childSnapshot.key,
+                        ...childSnapshot.val()
+                    });
+                });
+
+                dispatch(setExpense(expenses))
+            });
+    };
+};
--- src/app.js
+++ src/app.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpense } from './actions/expenses';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import './firebase/firebase'
 
 const store = configureStore();
 
@@ -16,4 +17,8 @@ const jsx =  (
     </Provider>
 );
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpense()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+})
