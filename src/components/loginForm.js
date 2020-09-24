b089866509bbdccdf81429c488047ae19215bf8d
aashiq_muha <aashiqmuhammad4@gmail>
2020-09-24 22:08:39 +0530

    security

---
+++ src/components/loginForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginFormPage = ({ startLogin}) => (
+    <div>
+        <button onClick={startLogin}> Log in</button>
+    </div>
+)
+const mapDispatchToProps = (dispatch) => ({
+    startLogin: () => dispatch(startLogin())
+})
+export default connect(undefined ,mapDispatchToProps)(LoginFormPage);
