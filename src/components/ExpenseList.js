638644e11bf9ddf6c1cfe140944d1c478bcf6a11
aashiq_muha <aashiqmuhammad4@gmail>
2020-09-28 13:53:15 +0530

    add styles

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -4,33 +4,32 @@ import ExpenseListItem from './ExpenseListItem';
 import SelectExpenses from '../selectors/expenses'
 
 const ExpenseList = (props) => (
-    <div className="content-container">
-        <div className="list-header">
-            <div className="show-for-mobile">Expenses</div>
-            <div className="show-for-desktop">Expense</div>
-            <div className="show-for-desktop">Amount</div>
-        </div>
-        <div className="list-body">
-            {props.expenses.length === 0 ? 
-                ( 
-                    <div className="list-item list-item__message">
-                        <p>No Expenses</p> 
-                    </div>
-                ) :
-                ( 
-                    props.expenses.map((expense) => { return <ExpenseListItem key={expense.id} { ...expense}/> })
-                )
-            }
-        </div>
-        
-    </div>
+	<div className="content-container">
+		<div className="list-header">
+			<div className="show-for-mobile">Expenses</div>
+			<div className="show-for-desktop">Expense</div>
+			<div className="show-for-desktop">Amount</div>
+		</div>
+		<div className="list-body">
+			{props.expenses.length === 0 ? 
+				( 
+					<div className="list-item list-item__message">
+						<p>No Expenses</p> 
+					</div>
+				) :
+				( 
+					props.expenses.map((expense) => { return <ExpenseListItem key={expense.id} { ...expense}/> })
+				)
+			}
+		</div>
+	</div>
 );
 
 const mapStateToProps = (state) => {
-    return {
-        expenses: SelectExpenses(state.expenses, state.filters),
-        filters: state.filters
-    };
+	return {
+		expenses: SelectExpenses(state.expenses, state.filters),
+		filters: state.filters
+	};
 }
 
 export default connect(mapStateToProps)(ExpenseList);
